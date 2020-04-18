6554327367e126198c322704a3b01b721428aa17
Fawez Essayes <58982115+fawezessayes@users.noreply.github.com>
2020-04-18 12:25:37 +0100

    Updates

---
+++ precache-manifest.efd9c40d283dcb7c25c13272acf4c7f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fb10db6672d844df36b8fbe42d5c0a2",
+    "url": "/jobs/index.html"
+  },
+  {
+    "revision": "99e2cfaf8dfa85fb8b8d",
+    "url": "/jobs/static/css/main.786e90ba.chunk.css"
+  },
+  {
+    "revision": "1d19ecb9d877c56fbe64",
+    "url": "/jobs/static/js/2.ce2b8206.chunk.js"
+  },
+  {
+    "revision": "210e5fea5d657e0a340abfa54200e342",
+    "url": "/jobs/static/js/2.ce2b8206.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99e2cfaf8dfa85fb8b8d",
+    "url": "/jobs/static/js/main.8c14638f.chunk.js"
+  },
+  {
+    "revision": "2f6d1e321f844d2b7be2",
+    "url": "/jobs/static/js/runtime-main.6781e7ea.js"
+  }
+]);
